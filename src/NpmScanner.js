f9f398b94de41b348b3e6cde1322e1a0d2b03707
khs4473 <zenparsing@gmail.com>
2014-01-03 12:47:31 -0500

    Progress

--- src/NpmScanner.js
+++ src/NpmScanner.js
@@ -1,26 +1,64 @@
-module HTTP from "node:https";
+module HTTP from "node:http";
 module HTTPS from "node:https";
 module URL from "node:url";
 module Path from "node:path";
+module FS from "node:fs";
 
 import { spawn } from "node:child_process";
 import { AsyncFS } from "package:zen-bits";
 
-function http(method, url, headers) {
+async traverseFileSystem(path, fn) {
+
+    path = Path.resolve(path);
+    
+    var list = [];
+    
+    try { list = await AsyncFS.readdir(path) }
+    catch (x) { }
+
+    for (var i = 0; i < list.length; ++i)
+        await traverseFileSystem(Path.join(path, list[i]), fn);
+    
+    if (await AsyncFS.exists(path))
+        await fn(path);
+}
+
+async wipeout(path) {
+
+    return traverseFileSystem(path, path => { 
+    
+        var stat;
+        
+        try { stat = await AsyncFS.stat(path) }
+        catch (x) { }
+        
+        if (!stat)
+            return;
+        
+        if (stat.isDirectory())
+            await AsyncFS.rmdir(path);
+        else
+            await AsyncFS.unlink(path);
+    });
+}
+
+async http(method, url, options) {
 
     return new Promise((resolve, reject) => {
     
-        var options = URL.parse(url),
+        options = options || {};
+        
+        var requestInfo = URL.parse(url),
             makeRequest;
         
-        switch (options.protocol.toLowerCase()) {
+        switch (requestInfo.protocol.toLowerCase()) {
         
             case "http:": makeRequest = HTTP.request; break;
             case "https:": makeRequest = HTTPS.request; break;
             default: throw new Error("Invalid protocol");
         }
         
-        headers = headers || {};
+        var headers = options.headers || {};
         
         if (!headers.Host)
             headers.Host = options.hostname;
@@ -28,13 +66,28 @@ function http(method, url, headers) {
         if (!headers.Cookie)
             headers.Cookie = "";
         
-        options.headers = headers || {};
+        requestInfo.headers = headers;
         
-        var body = "";
+        var request = makeRequest(requestInfo, response => {
 
-        var request = makeRequest(options, response => {
-
+            if (options.file) {
+            
+                if (Path.basename(options.file) === "*")
+                    options.file = options.file.replace(/\*$/, Path.basename(requestInfo.pathname));
+                
+                var filePath = Path.resolve(options.file);
+                var file = FS.createWriteStream(filePath);
+                
+                response.pipe(file);
+                file.on("finish", $=> resolve(filePath));
+                
+                return;
+            }
+            
+            var body = "";
+            
             response.setEncoding("utf8");
+                        
             response.on("data", data => body += data);
             response.on("end", $=> resolve(body));
         });
@@ -45,31 +98,26 @@ function http(method, url, headers) {
     
 }
 
-function NPM(target) {
+async unpack(path, dest) {
 
-    target = Path.resolve(target);
+    path = Path.resolve(path);
+    dest = Path.resolve(dest);
     
-    return async ƒ() {
+    return new Promise((resolve, reject) => {
     
-        var args = [].slice.call(arguments, 0);
-        
-        return new Promise((resolve, reject) => {
-    
-            var child = spawn("npm", args, {
-    
-                cwd: target,
-                env: process.env,
-                stdio: "inherit"
-            });
-        
-            child.on("exit", code => {
-        
-                if (code) reject(code);
-                else resolve(code);
-            });
+        var child = spawn("tar", ["xvfz", path], {
+
+            cwd: dest,
+            env: process.env,
+            stdio: "inherit"
         });
-        
-    };
+
+        child.on("exit", code => {
+
+            if (code) reject(code);
+            else resolve(code);
+        });
+    });
 }
 
 function rand(min, max) {
@@ -109,27 +157,32 @@ export class NpmScanner {
     
         if (!stat || !stat.isDirectory()) {
     
-            this.log(`Creating target directory [${ dest }].`);
+            this.log(`Creating target directory [${ dest }]`);
             await AsyncFS.mkdir(dest);
         }
     
-        this.log(`Writing empty package.json file at [${ dest }].`);
-        await AsyncFS.writeFile(Path.join(dest, "package.json"), "{}");
-    
-        this.log(`Fetching package index from NPM registry.`);
+        this.log(`Fetching package index from NPM registry (this may take several minutes)`);
         var response = await http("GET", "https://registry.npmjs.org/-/all");
     
         this.log(`Parsing JSON response`);
-        var data = JSON.parse(response);
+        var data; try { data = JSON.parse(response); }
+        catch (x) { console.log(response.slice(0, 512)); throw x; }
     
         var list = [], length = 0;
     
-        this.log(`Generating shuffled list of package names.`);
+        this.log(`Generating shuffled list of package names`);
         Object.keys(data).forEach(key => {
     
             if (key === "__updated")
                 return;
         
+            // Prefix with a dot if the package name is an integer.  This will
+            // force Node to maintain the correct enumeration order in the
+            // resulting object.
+            
+            if (key | 0 == key)
+                key = "." + key;
+                
             var r = rand(length);
         
             list[length++] = list[r];
@@ -140,6 +193,9 @@ export class NpmScanner {
         list.forEach(value => this.data[value] = null);
         
         this.packageList = list;
+        
+        // Since this operation is expensive, save before continuing
+        this.save();
     }
     
     async next() {
@@ -160,40 +216,72 @@ export class NpmScanner {
         if (this.position > this.packageList.length)
             return false;
         
-        var npm = NPM(this.folder);
+        // Remove leading dot from key name to get the real package name
+        var realName = name.replace(/^\./, "");
         
-        this.log(`Installing package ${ name }`);
+        this.log(`Processing package ${ realName }`);
         
-        try { 
-            
-            await npm("install", name);
-            
-        } catch (x) { 
+        var infoURL = `https://registry.npmjs.org/${ realName }/latest`;
+        this.log(`Downloading package metadata [${ infoURL }]`);
         
-            this.log(`Unable to install package ${ name }.`);
-            this.log(`Uninstalling package.`);
-            
-            try { await npm("rm", name) }
-            catch (x) { }
-            
-            this.data[name] = { error: "Unable to install package" };
-            return this.next();
-        }
+        var tarball = null;
         
         try {
         
-            var path = Path.join(this.folder, "node_modules");
-            path = Path.join(path, name);
+            var info = JSON.parse(await http("GET", infoURL));
             
-            this.data[name] = await this.process(name, path);
-        
-        } finally {
-        
-            this.log(`Uninstalling package.`);
+            if (!(info.dist && info.dist.tarball))
+                throw new Error("Metadata does not contain an archive reference");
             
-            try { await npm("rm", name) }
-            catch (x) { }
+            tarball = info.dist.tarball;
+        
+        } catch (x) {
+        
+            this.log(`Unable to retrieve metadata for package [${ realName }]`);
+            this.log(x.toString());
+            
+            // TODO: Error recovery?  Move to next?
+            throw x;
         }
+        
+        this.log(`Downloading package archive [${ tarball }]`);
+        var archive = null;
+        
+        try {
+        
+            archive = await http("GET", tarball, { file: Path.join(this.folder, "*") });
+        
+        } catch (x) {
+        
+            this.log(`Error occured while downloading archive`);
+            this.log(x.toString());
+            
+            // TODO: Error recovery?  Move to next?
+            throw x;
+        }
+        
+        var packageFolder = Path.join(this.folder, "package");
+    
+        this.log(`Clearing package folder [${ packageFolder }]`);
+    
+        await wipeout(packageFolder);
+        await AsyncFS.mkdir(packageFolder);
+    
+        this.log(`Unpacking archive [${ archive }]`);
+        await unpack(archive, packageFolder);
+        
+        this.log(`Removing archive file`);
+        await wipeout(archive);
+        
+        this.log(`Processing package [${ realName }]`);
+        var result = await this.processPackage(realName, packageFolder);
+        
+        if (result === void 0)
+            result = {};
+            
+        this.data[name] = result;
+        
+        return true;
     }
     
     async open() {
@@ -204,7 +292,7 @@ export class NpmScanner {
         var path = Path.join(this.folder, "data.json"),
             content = null;
         
-        this.log(`Attempting to read index file [${ path }].`);
+        this.log(`Attempting to read index file [${ path }]`);
         
         // Attempt to read the index file
         try { content = await AsyncFS.readFile(path) }
@@ -213,11 +301,11 @@ export class NpmScanner {
         // If the index does not exist, then attempt to initialize the folder
         if (content === null) {
         
-            this.log(`Index could not be read.  Attempting to initialize folder.`);
+            this.log(`Index could not be read - attempting to initialize folder`);
             return await this.initialize();
         }
         
-        this.log(`Parsing JSON package index.`);
+        this.log(`Parsing JSON package index`);
         
         this.data = JSON.parse(content);
         this.packageList = Object.keys(this.data);
@@ -228,15 +316,50 @@ export class NpmScanner {
         if (!this.data)
             throw new Error("Not open");
         
-        var data = this.data;
+        var p = this.save();
         this.data = null;
-        
-        await AsyncFS.writeFile(Path.join(this.folder, "data.json"), JSON.stringify(data));
+        await p;
     }
     
-    async process(name, path) {
+    async processPackage(name, path) {
+    
+        var data = {};
+        
+        await traverseFileSystem(path, path => {
+        
+            var stat;
+            
+            try { stat = await AsyncFS.stat(path) }
+            catch (x) { }
+            
+            if (!stat || !stat.isFile())
+                return;
+            
+            await this.processFile(name, path, data);
+        });
+        
+        return data;
+    }
+    
+    async processFile(packageName, path, data) {
     
         throw new Error("Not implemented");
+    }
+    
+    async save() {
+    
+        if (!this.data)
+            throw new Error("Package index not loaded");
+        
+        await AsyncFS.writeFile(Path.join(this.folder, "data.json"), JSON.stringify(this.data));
+    }
+    
+    reset() {
+    
+        if (!this.data)
+            throw new Error("Package index not loaded");
+        
+        Object.keys(this.data).forEach(key => this.data[key] = null);
     }
     
     log(msg) {
@@ -247,7 +370,9 @@ export class NpmScanner {
 
 export async main() {
 
-    var scanner = new NpmScanner("test/module-spec");
+    var scanner = new NpmScanner("_modulespec");
+    
+    await scanner.open();
     await scanner.next();
     await scanner.close();
 }
